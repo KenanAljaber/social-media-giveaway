ca38d8ca22c215f171c47d64cea5f4baa5ff24f1
KenanAljaber <keno12333@hotmail.com>
2023-07-17 19:56:22 +0200

    use the showWarningService and validate the post url

--- src/app/fetching/components/search-post-widget/search-post-widget.component.ts
+++ src/app/fetching/components/search-post-widget/search-post-widget.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { PostFetcherService } from '../../services/postfetcher.service';
 import { Comment } from '../../types/comment.interface';
+import { ShowWarningService } from 'src/app/shared/services/show-warning.service';
+import { MessageTypes } from 'src/app/shared/constants/CONSTANTS';
 
 @Component({
   selector: 'fetching-search-post-widget',
@@ -14,15 +16,30 @@ export class SearchPostWidgetComponent {
 
   comments:Comment[]=[];
 
-  constructor(private postFetcherService:PostFetcherService){}
+  constructor(
+    private postFetcherService:PostFetcherService,
+    private warningService:ShowWarningService
+      ){}
 
    searchPost(){
+    if(!this.postURL || this.postURL.trim().length==0 || !this.validatePostUrl(this.postURL)){
+      this.warningService.showWarning({message:"Please enter a valid post url",type:MessageTypes.WARNING,show:true})
+      return;
+    }
     this.postFetcherService.getCommentsByPostLink(this.postURL).subscribe(resp=>{
       this.comments=resp.comments;
       console.log(this.comments);
       
-    })
-      
+    })  
+  }
+
+  validatePostUrl(postUrl:string):boolean{
+        const regex:RegExp= /\/p\/([A-Za-z0-9_-]+)/;
+        const match= postUrl.match(regex);
+        if(match?.[1]){
+          return true;
+        }
+        return false;
   }
 
 }
